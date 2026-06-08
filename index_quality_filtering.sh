c71e7e537a2a41c1f2139d6273d639d89d3490c9
fitz-meyer <74560652+fitz-meyer@users.noreply.github.com>
2026-06-08 11:25:33 -0600

    commented out most echos, added skip step

--- index_quality_filtering.sh
+++ index_quality_filtering.sh
@@ -14,41 +14,46 @@ for i1 in `ls *_I1_001.fastq.gz`; do
     i2=${i1/_I1_001.fastq.gz/_I2_001.fastq.gz}
     r1=${i1/_I1_001.fastq.gz/_R1_001.fastq.gz}
     r2=${i1/_I1_001.fastq.gz/_R2_001.fastq.gz}
-        
+    
     # Generate the output filenames 
-    R1_iFILTERED=${r1/_R1_001.fastq.gz/_R1_ifiltered.fastq.gz}
+   	R1_iFILTERED=${r1/_R1_001.fastq.gz/_R1_ifiltered.fastq.gz}
     R2_iFILTERED=${r2/_R2_001.fastq.gz/_R2_ifiltered.fastq.gz}
-    
-    # Ensure the corresponding R2 file exists
-    if [[ -s $i2 && -s $r1 && -s $r2 ]]; then
-        echo "Averaging phred for $i1 and $i2"
-    
-    # Pass i1 and i2 variables to R script 
-    
-    	R --vanilla -f index_quality_filtering.R --args $i1 $i2
         
-    else
-        echo "Warning: Corresponding I2, R1, and R2 files not found for $i1"
-    fi
+    if [[ -s $R1_iFILTERED && -s $R2_iFILTERED ]]; then
+    	echo "Pair already filtered. Skipping."  
     
-    # Read in low quality reads list
-    
-    lowQ="./lowQ_reads.txt"
-    
-    if [[ -s $lowQ ]]; then
-        echo "Low quality reads list successfully generated"
-    else
-    	echo "FUCKTANGULAR!"
-    fi
-    
-    # Call filterbyname.sh
-    
-    conda run -n bbmap_env filterbyname.sh in=$r1 in2=$r2 out=$R1_iFILTERED out2=$R2_iFILTERED names=$lowQ -Xmx900m
-
-    if [[ -s $R1_iFILTERED && -s $R2_iFILTERED ]] ; then
-        echo "$i1 pair filtered successfully"
     else 
-    	echo "Filtering incomplete"
+    
+    	# Ensure the corresponding R2 file exists
+    	if [[ -s $i2 && -s $r1 && -s $r2 ]]; then
+        	#echo "Averaging phred for $i1 and $i2"
+        	
+    			# Pass i1 and i2 variables to R script 
+    			R --vanilla -f index_quality_filtering.R --args $i1 $i2
+        else
+        	#echo "Warning: Corresponding I2, R1, and R2 files not found for $i1"
+    	fi
+    
+    	# Read in low quality reads list
+    
+    	lowQ="./lowQ_reads.txt"
+    
+    	if [[ -s $lowQ ]]; then
+        	#echo "Low quality reads list successfully generated"
+    	else
+    		echo "FUCKTANGULAR!"
+    	fi
+    
+    	# Call filterbyname.sh
+    
+    	conda run -n bbmap_env filterbyname.sh in=$r1 in2=$r2 out=$R1_iFILTERED out2=$R2_iFILTERED names=$lowQ -Xmx900m
+
+    	if [[ -s $R1_iFILTERED && -s $R2_iFILTERED ]] ; then
+        	#echo "$i1 pair filtered successfully"
+    	else 
+    		#echo "Filtering incomplete"
+    	fi
+    
     fi
 
 done
@@ -59,4 +64,3 @@ mv *ifiltered.fastq.gz ifiltered_fastq/
 rm lowQ_reads.txt
 
 echo "bye"
-
